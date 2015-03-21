cbfc390f65c7e4682053f19d9b9af5e0eb930225
ChipH3rd <tlhazlett@cox.net>
2015-03-21 16:45:53 -0700

    Just starting. Two different types of Particles diffusing in a 2D grid. Collisions between them will be measured over time.

---
+++ Colliisons.R
@@ -0,0 +1,8 @@
+# Set up a 2D Grid with two different particles randomly moving around. Movment is in 8 directions. one step for each time cycle. 
+# particles that land in the same space get a chance (probability) to collide. Times (cycles) are input as a list and a Graph is
+# plotted as collisions as a function of time.
+
+particleA_Num <- 200
+ParticleB_Num <- 200
+Matrix_X <- 200
+Matrix_Y <- 200
